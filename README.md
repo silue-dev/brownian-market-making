507321eac9f6f176d17365b1634d51be2023d212
Bram Silue <bram@silue.dev>
2023-12-03 01:00:35 +0100

    added plots

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Market Making Algorithm Implementation
-This project focuses on implementing a basic market making algorithm for HFT.
+
+This project serves as a playground for implementing market making algorithms for High-Frequency Trading. The (final) model implemented in this project is the Avellaneda-Stoikov model. The research paper by Avellaneda and Stoikov can be accessed [here](https://citeseerx.ist.psu.edu/document?repid=rep1&type=pdf&doi=93e392c5b2765e6691b8acb60cd4a7e975bf1d6a). 
+
+To execute the code, simply run:
+
+```
+python src/main.py
+```
+
+## Result
+The image below is an example run of the market maker.
+
+![result](img/result.png)
--- src/plot.py
+++ src/plot.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
 def plot_performance(t, s, r, r_a, r_b, q, pnls):
-    f = plt.figure(figsize=(24, 4))
+    f = plt.figure(figsize=(12, 12))  # Adjusted figure size for square formation
 
     # Final run prices
-    ax1 = f.add_subplot(1, 4, 1)
+    ax1 = f.add_subplot(2, 2, 1)  # Changed to 2x2 grid, position 1
     plt.plot(t, s, color='black', label='Market price')
     plt.plot(t, r, color='blue', linestyle='dashed', label='Reservation price')
     plt.plot(t, r_a, color='red', linestyle='', marker='x', markersize='2', label='Ask price')
@@ -16,7 +16,7 @@ def plot_performance(t, s, r, r_a, r_b, q, pnls):
     ax1.set_title("Final Run Prices", fontweight='bold')
 
     # Final run PnL
-    ax2 = f.add_subplot(1, 4, 2)
+    ax2 = f.add_subplot(2, 2, 2)  # Position 2
     plt.plot(t, pnls[-1], color='green', label='PnL')
     plt.xlabel('Time', fontweight='bold')
     plt.ylabel('PnL', fontweight='bold')
@@ -25,7 +25,7 @@ def plot_performance(t, s, r, r_a, r_b, q, pnls):
     ax2.set_title("Final Run PnL", fontweight='bold')
 
     # Final run inventory
-    ax3 = f.add_subplot(1, 4, 3)
+    ax3 = f.add_subplot(2, 2, 3)  # Position 3
     plt.plot(t, q, color='blue', label='Stocks held')
     plt.xlabel('Time', fontweight='bold')
     plt.ylabel('Inventory', fontweight='bold')
@@ -34,7 +34,7 @@ def plot_performance(t, s, r, r_a, r_b, q, pnls):
     ax3.set_title("Final Run Inventory", fontweight='bold')
 
     # PnL distribution
-    ax4 = f.add_subplot(1, 4, 4)
+    ax4 = f.add_subplot(2, 2, 4)  # Position 4
     plt.hist(pnls[:,-1])
     plt.xlabel('PnL', fontweight='bold')
     plt.ylabel('Frequency', fontweight='bold')
