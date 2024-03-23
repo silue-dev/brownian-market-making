8f5bb58cecf73ea7a526701999a2ac6b62b3a013
Bram Silue <bram@silue.dev>
2024-03-23 15:05:20 +0100

    moved data to dedicated data directory

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@
 import sqlite3
 
 class Database:
-    def __init__(self, db_path='src/database.db'):
+    def __init__(self, db_path='src/data/database.db'):
         self.db_path = db_path
         self.connection = None
         self.cursor = None
